09cda08c01a7ea567e09b8877e49fd1048b644e6
daghankilicay <daghankilicay@hotmail.com>
2020-02-20 17:01:20 +0300

    Initial Commit

---
+++ TodayInHistoryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodayInHistoryApp
+//
+//  Created by Dağhan KILIÇAY on 20.02.2020.
+//  Copyright © 2020 Dağhan KILIÇAY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
